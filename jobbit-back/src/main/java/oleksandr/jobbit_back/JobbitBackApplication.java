f6fc86d55ec32e039560125e1b0d18179a787b63
Oleksandr Borovyk <sasha.borovyk03@gmail.com>
2025-02-27 19:37:50 +0200

    1. Initializing my project

---
+++ jobbit-back/src/main/java/oleksandr/jobbit_back/JobbitBackApplication.java
@@ -0,0 +1,13 @@
+package oleksandr.jobbit_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobbitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobbitBackApplication.class, args);
+	}
+
+}
---
+++ jobbit-back/src/test/java/oleksandr/jobbit_back/JobbitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package oleksandr.jobbit_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobbitBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
